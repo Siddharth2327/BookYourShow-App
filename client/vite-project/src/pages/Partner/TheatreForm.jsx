b68a1f828cfd009f5e7806ec761fe37eda1c196c
Siddharth <msiddharth298@gmail.com>
2025-08-11 14:29:20 +0530

    Small correction

--- client/vite-project/src/pages/Partner/TheatreForm.jsx
+++ client/vite-project/src/pages/Partner/TheatreForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Form, Input, InputNumber, Select, Space } from 'antd';
+import { Button, Form, Input, Space } from 'antd';
 import { useEffect } from 'react';
 
 const layout = {
